8b69b3923886a479a80756c6c4b6d94ba8d79869
Harsh Kumar <meharshkumar1@gmail.com>
2017-10-05 00:32:05 +0530

    added jquery slide effect

--- routes/index.js
+++ routes/index.js
@@ -10,16 +10,95 @@ router.use(session({
   resave: false,
   saveUninitialized: true
 }))
-router.use(bodyParser.json()) ; // useful for parsing the ajax post data
+//router.use(bodyParser.json()) ; // useful for parsing the ajax post data
 router.use(function (req, res, next) {
-  if (!req.session.name) {
-    req.session.name = " " ;
+  if (!req.session.username) {
+    req.session.usercreated = false ;
+    req.session.userexists = false ;
+    req.session.username = " ";
+    req.session.loginallowed = false ;
   }
   next()
 })
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(__dirname+'/views/index.html');
-});
+  req.session.destroy() ;
+})
 
+router.get('/main.css', function(req, res) {
+  res.sendFile(__dirname+'/views/main.css') ;
+})
+
+router.get('/frontend.js', function(req, res) {
+  res.sendFile(__dirname+'/views/frontend.js') ;
+})
+
+router.get('/createUser', function(req, res) {
+  var object = {} ;
+  console.log(req.session.usercreated) ;
+  if(req.session.usercreated) {
+    object.message = "User has been created. Now, please login" ;
+  }
+  else {
+    object.message = "The user already exists." ;
+  }
+  console.log(object) ;
+  res.jsonp(object) ;
+})
+
+router.get('/checkUserLogin', function(req, res) {
+  var object = {} ;
+  if(req.session.loginallowed) {
+    object.value = "OK" ;
+    object.username = req.session.username ;
+  }
+  res.jsonp(object) ;
+  console.log(object) ;
+})
+
+router.post('/createUser', function(req, res) {
+  mongo.connect(mongourl, function(err, db) {
+    var collection = db.collection('usersfornightlife') ;
+    console.log(req.body) ;
+    console.log(req.body._id) ;
+    collection.find({"_id":req.body._id}).toArray(function(err, docs) {
+      console.log(docs) ;
+      if(docs.length<1) {
+        console.log("the user does not exist") ;
+          collection.insert(req.body) ;
+          req.session.usercreated = true ;
+          console.log(req.session.usercreated) ;
+          db.close() ;
+          res.sendStatus(200) ;
+      }
+      else {
+        res.sendStatus(500) ;
+        db.close() ;
+      }
+    })
+  })
+})
+router.post('/checkUserLogin', function(req, res) {
+  mongo.connect(mongourl, function(err, db) {
+    let collection = db.collection('usersfornightlife') ;
+    console.log(req.body) ;
+    collection.find({"_id": req.body._id}).toArray(function(err, docs) {
+      if(docs.length<1) {
+        // user does not exist ;
+        req.session.userexists = false ;
+        res.sendStatus(200) ;
+        console.log('The user does not exit') ;
+      }
+      else {
+        if(req.body.password===docs[0]["password"]) {
+          req.session.username = docs[0]["username"] ;
+          req.session.loginallowed = true ;
+          res.sendStatus(200) ;
+        }
+      }
+      db.close() ;
+    })
+  })
+})
 module.exports = router;
--- routes/views/frontend.js
+++ routes/views/frontend.js
@@ -10,12 +10,17 @@ function processSignup() {
     data: JSON.stringify(object),
     contentType: "application/json",
     success: function() {
+      console.log('success');
       $.ajax({
         url: 'createUser',
         type: 'GET',
         dataType: 'jsonp',
         success: function(data) {
-          document.getElementById('signupmessage').innerHTML = data.message ;
+          var a = document.getElementById('signupdropdown') ;
+          a.style.backgroundColor = "white" ;
+          a.style.color = "green" ;
+          a.innerHTML = data.message ;
+          console.log(data.message) ;
         }
       }) ;
     },
@@ -36,11 +41,14 @@ function checkLogin() {
     contentType: 'application/JSON',
     data: JSON.stringify(object),
     success: function() {
+      console.log('success') ;
       $.ajax({
         url: 'checkUserLogin',
         type: "GET",
         dataType: 'jsonp',
         success: function(value) {
+          console.log(value.value);
+          console.log(value.username);
           if(value.value==="OK") {
             document.getElementById('loginbutton').innerHTML = value.username ;
           }
@@ -52,3 +60,36 @@ function checkLogin() {
     }
   }) ;
 }
+
+
+function search() {
+  let location = document.getElementById('places').value ;
+  $.ajax({url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBoCRRbEs84If1UsBGQyzStE9b9njaxoAo',
+            dataType: "JSON"  ,
+            success: function(data) {
+                console.log(data) ;
+                if(data.status == "OK") {
+                  $.ajax({
+                    url:'https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d11f941735,4bf58dd8d48988d123941735,56aa371be4b08b9a8d57354d,4bf58dd8d48988d11b941735,52e81612bcbc57f1066b7a0d,56aa371ce4b08b9a8d57356c,4bf58dd8d48988d1d5941735,4bf58dd8d48988d122941735,4bf58dd8d48988d121941735,53e510b7498ebcb1801b55d4,4bf58dd8d48988d11a941735&ll='+data.results[0].geometry.location.lat+","+data.results[0].geometry.location.lng+'&radius=10000&intent=browse&v=20120801&client_id=CZWUOXHDACJSHFEYTBZZ2304SBXRZEQCGXA5ER5D3MF0ALYZ&client_secret=Z4LJLNABYG2SVWZVBR4KEUJPQ4A5ROPM5I2YPWMZP04AHDE4&v=20170501',
+                    success: function(results) {
+                      console.log(results);
+                      var size = results.responses.venues.length ;
+                    }
+                  }) ;
+                }
+            }
+  }) ;
+}
+
+$(document).ready(function() {
+    $('#loginbutton').click(
+        function(){
+            $(this).children('#logindropdown').slideToggle(500);
+        }
+    );
+    $('#signupbutton').click(
+        function() {
+            $(this).children('#signupdropdown').slideToggle(500) ;
+        }
+    )
+});
