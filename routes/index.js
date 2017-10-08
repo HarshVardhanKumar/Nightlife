afa78eb8271f77093429543336494c500082b897
Harsh Kumar <meharshkumar1@gmail.com>
2017-10-08 23:57:54 +0530

    added spinners

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.use(function (req, res, next) {
     req.session.userexists = false ;
     req.session.username = " ";
     req.session.loginallowed = false ;
+    req.session.email_id = " " ;
   }
   next()
 })
@@ -57,9 +58,37 @@ router.get('/checkUserLogin', function(req, res) {
   console.log(object) ;
 })
 
+router.get('/placesnightlife_noofusers', function(req,res) {
+  mongo.connect(mongourl, function(err, db) {
+    if(err) {
+      res.sendStatus(500) ;
+      db.close() ;
+    }
+    else {
+      let collection = db.collection('placesnightlife_noofusers') ;
+      collection.find().toArray(function(err, docs) {
+        if(err) {
+          res.sendStatus(500) ;
+          db.close() ;
+        }
+        else {
+          console.log(docs);
+          let response = {} ;
+          for(let i = 0 ; i<docs.length ; i++) {
+            response[docs[i]._id] = docs[i].noofusers ;
+          }
+          res.jsonp(response) ;
+          db.close() ;
+        }
+      })
+    }
+  })
+})
 router.post('/createUser', function(req, res) {
   mongo.connect(mongourl, function(err, db) {
+  if(db!==null) {
     var collection = db.collection('usersfornightlife') ;
+    let collection1 = db.collection('usersnightlife_places') ;
     console.log(req.body) ;
     console.log(req.body._id) ;
     collection.find({"_id":req.body._id}).toArray(function(err, docs) {
@@ -67,38 +96,155 @@ router.post('/createUser', function(req, res) {
       if(docs.length<1) {
         console.log("the user does not exist") ;
           collection.insert(req.body) ;
+
+          let user = {} ;
+          user._id = req.body._id ;
+          user.venues_visiting = new Array(0) ;
+          collection1.insert(user) ;
+
           req.session.usercreated = true ;
           console.log(req.session.usercreated) ;
           db.close() ;
-          res.sendStatus(200) ;
+
+          var object = {} ;
+          console.log(req.session.usercreated) ;
+          if(req.session.usercreated) {
+            object.message = "User has been created. Now, please login" ;
+          }
+          else {
+            object.message = "The user already exists." ;
+          }
+          console.log(object) ;
+          res.jsonp(object) ;
+
       }
       else {
         res.sendStatus(500) ;
         db.close() ;
       }
     })
+  }
   })
 })
+
 router.post('/checkUserLogin', function(req, res) {
   mongo.connect(mongourl, function(err, db) {
+  if(db!==null) {
     let collection = db.collection('usersfornightlife') ;
     console.log(req.body) ;
     collection.find({"_id": req.body._id}).toArray(function(err, docs) {
       if(docs.length<1) {
         // user does not exist ;
         req.session.userexists = false ;
-        res.sendStatus(200) ;
         console.log('The user does not exit') ;
+        var object = {} ;
+        if(req.session.loginallowed) {
+          object.value = "NOT" ;
+          object.username = undefined ;
+        }
+        res.jsonp(object) ;
       }
       else {
         if(req.body.password===docs[0]["password"]) {
           req.session.username = docs[0]["username"] ;
+          req.session.email_id = docs[0]["_id"] ;
           req.session.loginallowed = true ;
-          res.sendStatus(200) ;
+          var object = {} ;
+          if(req.session.loginallowed) {
+            object.value = "OK" ;
+            object.username = req.session.username ;
+          }
+          res.jsonp(object) ;
         }
       }
       db.close() ;
     })
+  }
   })
 })
+
+router.post('/createCheckin', function(req, res) {
+  mongo.connect(mongourl, function(err, db) {
+  if(db!==null) {
+    let venueid = req.body.venueid ;
+    let collection1 = db.collection('usersnightlife_places') ;
+    let collection2 = db.collection('placesnightlife_noofusers') ;
+    // we first determine whether the user is already going to this place.
+
+    collection1.find({"_id": req.session.email_id}).toArray(function(err, docs) {
+        // the user already exits, so we check whether he/she is going to the venue ;
+        console.log(req.session.email_id);
+        console.log(docs);
+        if(docs[0].venues_visiting.indexOf(req.body.venueid)>-1) {
+          // the user is already going there
+          var response = {} ; response.updatedvalue = "initialvalue"; response.message = "Already going there" ; // response.updatedvalue = "initialvalue" indicates the frontend to maintain the same value.
+          res.jsonp(response) ;
+          db.close() ;
+        }
+        else {
+          // the user is not visiting there
+          // increase the no. of users visiting the places, then make an entry for the user's venues list
+          // check if the venueid exits in collection2. If not, then make an entry
+          let transactionsuccessful = true ;
+          // find if the venue exists in the second collection.
+          collection2.find({"_id": venueid}).toArray(function(err, doc) {
+            if(doc.length>0) {
+              // the venue exits. Now update the value
+              collection2.update({"_id": venueid}, {$inc: {"noofusers": 1}}, function(err) {
+                //some error occured. now abort the transaction.
+                if(err) {
+                  transactionsuccessful = false ;
+                  res.sendStatus(400) ;
+                }
+                else {
+                  docs[0].venues_visiting.push(venueid) ; // doc here refers to collection2 whereas docs refers to collection1
+                  collection1.update({"_id": req.session.email_id}, {$set: {"venues_visiting": docs[0].venues_visiting}}, function(err) {
+                    if(err) {
+                      console.log('some error occured');
+                      res.sendStatus(400) ;
+                    }
+                    else {
+                      var response = {} ; response.updatedvalue = "increasevalue";// response.updatedvalue = "initialvalue" indicates the frontend to maintain the same value.
+                      res.jsonp(response) ;
+                    }
+                  }) ;
+                }
+              })
+            }
+            else {
+              // the venue is not listed in placesnightlife_noofusers table.
+              let object = {} ;
+              object._id = req.body.venueid ;
+              console.log('venueid is '+venueid);
+              object.noofusers  = 1 ;
+              console.log(object);
+              collection2.insert(object, function(err, data) {
+                if(err) {
+                  // notify the frontend of error
+                  res.sendStatus(400) ;
+                }
+                else {
+                  docs[0].venues_visiting.push(venueid) ;
+                  console.log('inserted a new venue');
+                  collection1.update({"_id": req.session.email_id}, {$set: {"venues_visiting": docs[0].venues_visiting}}, function(err) {
+                    if(err) {
+                      console.log('some error occured');
+                      res.sendStatus(400) ;
+                    }
+                    else {
+                      var response = {} ; response.updatedvalue = "updatevalue"; // response.updatedvalue = "initialvalue" indicates the frontend to maintain the same value.
+                      res.jsonp(response) ;
+                    }
+                  }) ;
+                }
+              })
+            }
+          })
+
+        }
+      })
+    }
+    })
+  })
+
 module.exports = router;
--- routes/views/frontend.js
+++ routes/views/frontend.js
@@ -1,3 +1,21 @@
+let loggedinuser = false ;
+let notificationhtml = "<div class='notification'><span class='closebutton' onclick='close()'>&times;</span>$  ^</div><div ></div>" ;
+let originalbodycontent = "" ;
+window.onload = function() {
+  originalbodycontent = document.getElementsByTagName('body')[0].innerHTML;
+}
+
+function showLoader(loaderid) {
+  var loader = document.getElementById(loaderid) ;
+  loader.style.position = "relative" ;
+  loader.style.top = loader.parentElement.clientHeight*0.45 ;
+  loader.style.display = "block" ;
+}
+
+function hideLoader(loaderid) {
+  document.getElementById(loaderid).style.display = "none" ;
+}
+
 function processSignup() {
   var object = {} ;
   object.username=document.getElementById('signupusername').value ;
@@ -9,20 +27,20 @@ function processSignup() {
     type: "POST",
     data: JSON.stringify(object),
     contentType: "application/json",
-    success: function() {
+    success: function(data) {
       console.log('success');
-      $.ajax({
-        url: 'createUser',
-        type: 'GET',
-        dataType: 'jsonp',
-        success: function(data) {
+      //$.ajax({
+        //url: 'createUser',
+        //type: 'GET',
+        //dataType: 'jsonp',
+        //success: function(data) {
           var a = document.getElementById('signupdropdown') ;
           a.style.backgroundColor = "white" ;
           a.style.color = "green" ;
           a.innerHTML = data.message ;
           console.log(data.message) ;
-        }
-      }) ;
+        //}
+      //}) ;
     },
     error: function() {
       document.getElementById('signupmessage').innerHTML = "Signup Failed. Please Try again." ;
@@ -34,62 +52,183 @@ function checkLogin() {
   var object = {} ;
   object._id = document.getElementById('loginemail').value ;
   object.password = document.getElementById('loginpassword').value ;
+  document.getElementById('loginemail').value = "" ;
+  document.getElementById('loginpassword').value = "" ;
 
   $.ajax({
     url: 'checkUserLogin',
     type:"POST",
     contentType: 'application/JSON',
     data: JSON.stringify(object),
-    success: function() {
-      console.log('success') ;
-      $.ajax({
-        url: 'checkUserLogin',
-        type: "GET",
-        dataType: 'jsonp',
-        success: function(value) {
-          console.log(value.value);
-          console.log(value.username);
+    success: function(value) {
+
           if(value.value==="OK") {
-            document.getElementById('loginbutton').innerHTML = value.username ;
+            loginclick() ;
+            console.log(document.getElementById('loginbutton').clientWidth) ;
+            setTimeout(function() {
+              document.getElementById('loginbutton').innerHTML = value.username ;}, 500) ;
+            let totalleftvaluesoccupiedbysignupandlogin = document.getElementById('signupbutton').offsetWidth+document.getElementById('loginbutton').offsetWidth ;
+            console.log(totalleftvaluesoccupiedbysignupandlogin);
+            $('ul li:first-child').css("padding-right",document.documentElement.clientWidth-totalleftvaluesoccupiedbysignupandlogin-500) ;
+            loggedinuser = true ;
           }
           else {
             document.getElementById('loginmessage').innerHTML = "Login Failed" ;
           }
-        }
-      }) ;
+        //}
+      //}) ;
     }
   }) ;
 }
 
+var venuesearchurlprefix = "https://api.foursquare.com/v2/venues/";
+var venuesearchurlsuffix = "?client_id=CZWUOXHDACJSHFEYTBZZ2304SBXRZEQCGXA5ER5D3MF0ALYZ&client_secret=Z4LJLNABYG2SVWZVBR4KEUJPQ4A5ROPM5I2YPWMZP04AHDE4&v=20170501" ;
 
 function search() {
+
   let location = document.getElementById('places').value ;
-  $.ajax({url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBoCRRbEs84If1UsBGQyzStE9b9njaxoAo',
-            dataType: "JSON"  ,
-            success: function(data) {
-                console.log(data) ;
-                if(data.status == "OK") {
-                  $.ajax({
-                    url:'https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d11f941735,4bf58dd8d48988d123941735,56aa371be4b08b9a8d57354d,4bf58dd8d48988d11b941735,52e81612bcbc57f1066b7a0d,56aa371ce4b08b9a8d57356c,4bf58dd8d48988d1d5941735,4bf58dd8d48988d122941735,4bf58dd8d48988d121941735,53e510b7498ebcb1801b55d4,4bf58dd8d48988d11a941735&ll='+data.results[0].geometry.location.lat+","+data.results[0].geometry.location.lng+'&radius=10000&intent=browse&v=20120801&client_id=CZWUOXHDACJSHFEYTBZZ2304SBXRZEQCGXA5ER5D3MF0ALYZ&client_secret=Z4LJLNABYG2SVWZVBR4KEUJPQ4A5ROPM5I2YPWMZP04AHDE4&v=20170501',
-                    success: function(results) {
-                      console.log(results);
-                      var size = results.responses.venues.length ;
+
+  document.getElementsByTagName('body')[0].innerHTML = originalbodycontent ;
+  document.getElementById('body').style.display="none" ;
+
+  showLoader("loader") ;
+
+  $.ajax({
+    url: 'placesnightlife_noofusers',
+    method: 'GET',
+    dataType: 'jsonp',
+    success: function(resultsplacesnightlife) {
+      //  results contain the details of the venues existing in the database and no. of users
+      // now get the results of the query of location
+      // we then compare the venueid of the results of location with the data and set the going button inner html.
+      $.ajax({url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBoCRRbEs84If1UsBGQyzStE9b9njaxoAo',
+                dataType: "JSON"  ,
+                success: function(data) {
+                  // the longitude and latitude data is obtained for the given location
+                    console.log(data) ;
+                    if(data.status == "OK") {
+
+                      $.ajax({
+                        url:'https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d11f941735,4bf58dd8d48988d123941735,56aa371be4b08b9a8d57354d,4bf58dd8d48988d11b941735,52e81612bcbc57f1066b7a0d,56aa371ce4b08b9a8d57356c,4bf58dd8d48988d1d5941735,4bf58dd8d48988d122941735,4bf58dd8d48988d121941735,53e510b7498ebcb1801b55d4,4bf58dd8d48988d11a941735&ll='+data.results[0].geometry.location.lat+","+data.results[0].geometry.location.lng+'&radius=10000&intent=browse&v=20120801&client_id=CZWUOXHDACJSHFEYTBZZ2304SBXRZEQCGXA5ER5D3MF0ALYZ&client_secret=Z4LJLNABYG2SVWZVBR4KEUJPQ4A5ROPM5I2YPWMZP04AHDE4&v=20170501',
+                        success: function(results) {
+                          // the venueid of the query results have been obtained. we now make separate request for each of the venueid to get the details of the venue.
+                          console.log(results);
+                          var size = results.response.venues.length ;
+                          setTimeout( function() {
+                            for(let i = 0 ; i<size ; i++) {
+                              $.ajax({
+                                url: venuesearchurlprefix+results.response.venues[i].id+venuesearchurlsuffix ,
+                                dataType: 'json',
+                                success: function(data) {
+                                  // The details of the venue have been obtained.
+                                  console.log(data);
+
+                                  if(data.meta.code===200) {
+                                    let url = data.response.venue.canonicalUrl ;
+                                    let imageurl = data.response.venue.bestPhoto.prefix+'100x100'+data.response.venue.bestPhoto.suffix ;
+                                    let rating = data.response.venue.rating+'/10' ;
+
+                                    if(data.response.venue.rating===undefined) {
+                                      rating = "Not available" ;
+                                    }
+
+                                    let locationdetails = data.response.venue.location.formattedAddress.join(', ') ;
+                                    let contactdetails = data.response.venue.contact.phone ;
+
+                                    if(data.response.venue.contact.phone===undefined) {
+                                      contactdetails = "Not available" ;
+                                    }
+
+                                    let noofusers = resultsplacesnightlife[data.response.venue.id] ;
+                                    if(noofusers===undefined) {
+                                      noofusers = 0 ;
+                                    }
+                                    console.log('noofusers is '+noofusers);
+                                    document.getElementById('body').innerHTML+='<div class="venues" id="g'+i+'" ><img src='+imageurl+' class = "venueimage"><div class="venuedescription"> <b>'+data.response.venue.name+'</b><span class="ratings"> ratings: '+rating+'</span>    <p>    Location: '+locationdetails+'<br>      Contacts: '+contactdetails+'<br><br><a href="'+url+'">Visit Page</a>  </p>  </div><div class="authorizedcheckins"> '+data.response.venue.stats.usersCount+' Past Checkins </div><div '+' class = "goingbutton" onclick = addcheckin('+'"'+data.response.venue.id+'"'+','+i+') id="a'+i+'" >'+noofusers+'<i class="fa fa-spinner" style="font-size:15px; display:none" aria-hidden="true"></i> Going  </div></div>' ;
+                                  }
+
+                                  document.getElementById('loader').style.display = "none" ;
+                                  document.getElementById('body').style.display="block" ;
+                                  document.getElementById('searchplaces').style.paddingTop = 50 ;
+                                  document.getElementById('places').value = location ;
+
+                                }
+                              }) ;
+
+                            }
+                          },1500) ;
+                        },
+                        error: function(err) {
+                          document.getElementById('body').style.display = "block" ;
+                          document.getElementsByTagName('body')[0].innerHTML+=err.toString() ;
+                        }
+                      }) ;
                     }
-                  }) ;
-                }
-            }
+    },
+    error: function() {
+      document.getElementById('places').value = "Please Enter the postal address of your location"
+    }
+  });
+  document.getElementById('placeslabel').style.display = "none" ;
+  //console.log(document.getElementsByTagName('body')[0].innerHTML) ;
+  }
   }) ;
 }
 
-$(document).ready(function() {
-    $('#loginbutton').click(
-        function(){
-            $(this).children('#logindropdown').slideToggle(500);
+function goto(url) {
+  console.log('url is '+url);
+  window.location = url ;
+}
+function addcheckin(id, i) {
+  id = id.trim() ;
+  if(loggedinuser===false) {
+    loginclick() ;
+  }
+  else {
+    id = id.toString()+"" ;
+    console.log(document.getElementById("a"+i).children);
+    var checkinurl = "createCheckin";
+    var object = {} ;
+    object.venueid = id ;
+    $.ajax({
+      url : checkinurl,
+      method: "POST",
+      contentType: 'application/json',
+      data: JSON.stringify(object),
+      success: function(data) {
+        console.log('done');
+        console.log(data);
+        let searchg = document.createElement("p") ; searchg.setAttribute("class", "searchg") ;
+        if(data.updatedvalue!=="initialvalue") {
+          let initialvalue = parseInt(document.getElementById("a"+i).innerHTML.split(" ")[0]) ;
+          // show notification that the venue has been added to the user list
+          searchg.innerHTML = "Venue added to your list" ;
+          document.getElementById("g"+i).appendChild(searchg) ;
+          document.getElementById("a"+i).innerHTML = (initialvalue+1)+'<i class="fa fa-spinner" style="font-size:15px; display:none" aria-hidden="true"></i> Going' ;
+
         }
-    );
-    $('#signupbutton').click(
-        function() {
-            $(this).children('#signupdropdown').slideToggle(500) ;
+        else {
+          // show the notification that this user is already going that place.
+          searchg.innerHTML = "You are already going here" ;
+          document.getElementById("g"+i).appendChild(searchg) ;
         }
-    )
-});
+      }
+    }) ;
+  }
+}
+
+function setcontentofalertbars(alerthtml, contentbeforeclosebutton) {
+  var firstcontent = alerthtml.split('$')[0] ;
+  var lastcontent = alerthtml.split('^')[1] ;
+
+  return firstcontent+contentbeforeclosebutton+lastcontent ;
+}
+
+function loginclick() {
+            $('#signupdropdown').slideUp(500) ;
+            $('#logindropdown').slideToggle(500);
+}
+function signupclick() {
+            $('#logindropdown').slideUp(500);
+            $('#signupdropdown').slideToggle(500) ;
+}
